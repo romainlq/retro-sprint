c4571a7d1f92a5ab11ac52fb76e6e060cf462bf5
Romain Le Quellec <romain.lequellec-ext@elis.com>
2018-12-24 14:14:37 +0100

    init commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="full-width center-content">
+    <hello-component name="Pouet"/>
+  </div>
+</template>
+
+<script>
+
+import HelloComponent from './components/HelloComponent.vue'
+export default {
+  components: {
+    HelloComponent
+  }
+}
+</script>
